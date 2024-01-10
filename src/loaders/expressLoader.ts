c82d3cf7797894b485037a6a35ec9bf9482d0cbf
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-09 20:36:54 -0700

    explicit controllers & middlewares loading

--- src/loaders/expressLoader.ts
+++ src/loaders/expressLoader.ts
@@ -1,10 +1,13 @@
 import { Application as express } from "express";
 import { useExpressServer } from "routing-controllers";
+import IndexController from "../controllers/IndexController";
+import CompressionMiddleware from "../middlewares/CompressionMiddleware";
+import LogMiddleware from "../middlewares/LogMiddleware";
 
 export const expressLoader = (app: express): void => {
     useExpressServer(app, {
-        controllers: [`${__dirname}/../controllers/*.ts`],
-        middlewares: [`${__dirname}/../middlewares/*.ts`],
+        controllers: [IndexController],
+        middlewares: [CompressionMiddleware, LogMiddleware],
     });
 };
 
