2b7420e94fb1cc914382f6cc9500759b1b706171
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-14 13:50:29 -0700

    gracefull shutdown; process signal handlers added

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
 import 'reflect-metadata';
 import application from "./application";
+import * as process from "process";
 
-const app = application()
-    .then(({ server, port, logger }) => server.listen(port, () => logger.info(`Server is running on port ${port}`)))
+application()
+    .then(({ app, dataSource, port, logger }) => {
+        const server = app.listen(port, () => logger.info(`Server is running on port ${port}`));
+
+        const closeProcesses = (code: number = 1) => {
+            server.close(() => {
+                logger.info('Server closed');
+                dataSource.destroy().then(() => {
+                    logger.info('Database connection closed');
+                    process.exit(code);
+                });
+            });
+        }
+        const failureHandler = (error: string) => {
+            logger.error(error);
+            closeProcesses(1);
+        };
+
+        process.on('uncaughtException', failureHandler);
+        process.on('unhandledRejection', failureHandler);
+
+        const successHandler = () => {
+            logger.info('SIGTERM received');
+            closeProcesses(0);
+        };
+
+        process.on('SIGTERM', successHandler);
+    })
     .catch(error => console.log(error))
 ;
