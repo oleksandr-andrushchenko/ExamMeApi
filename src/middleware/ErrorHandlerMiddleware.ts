d62bc1b5426edf70eb665960058bf6d7b0d46297
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-14 14:37:27 -0700

    error handler added

---
+++ src/middleware/ErrorHandlerMiddleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import { ExpressErrorMiddlewareInterface, HttpError, Middleware } from 'routing-controllers';
+import { Inject, Service } from "typedi";
+import { LoggerInterface } from "../logger/LoggerInterface";
+
+@Service()
+@Middleware({ type: 'after' })
+export class ErrorHandlerMiddleware implements ExpressErrorMiddlewareInterface {
+
+    constructor(
+        @Inject('env') private readonly env: string,
+        @Inject('logger') private readonly logger: LoggerInterface,
+    ) {
+    }
+
+    public error(error: HttpError, req: Request, res: Response, next: NextFunction): void {
+        res.status(error.httpCode || 500).json({
+            name: error.name,
+            message: error.message,
+            errors: error[`errors`] || [],
+        });
+
+        if (this.env === 'production') {
+            this.logger.error(error.name, error.message);
+        } else {
+            this.logger.error(error.name, error.stack);
+        }
+    }
+}
