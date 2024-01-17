4071ec5cbeefe6bdac1db96aed10461849581608
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-16 22:04:27 -0700

    wrong entity bug fixed; find one by id method added

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,14 +1,18 @@
 import { MongoRepository } from "typeorm";
+import { ObjectId as MongoObjectId } from "mongodb";
 import { Service } from "typedi";
 import User from "../entity/User";
-import Category from "../entity/Category";
 import createRepository from "../util/createRepository";
 
 // @ts-ignore
-const create = () => createRepository(Category, (...args) => new UserRepository(...args));
+const create = () => createRepository(User, (...args) => new UserRepository(...args));
 
 @Service({ factory: create })
 export default class UserRepository extends MongoRepository<User> {
+
+    public async findOneById(id: string): Promise<User | null> {
+        return await this.findOneBy({ _id: new MongoObjectId(id) });
+    }
 
     public async findOneByEmail(email: string): Promise<User | null> {
         return await this.findOneBy({ email });
