2197b7b095ff89573267b73e7eae314a2bf16353
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-16 22:10:13 -0700

    generate access/refresh token method: expires param added; verify access token method added; verify token method: splitted into verify token and get verified token methods; issues fixed; refactoring

--- src/service/token/TokenService.ts
+++ src/service/token/TokenService.ts
@@ -13,6 +13,7 @@ export type GeneratedToken = {
 export type TokenPayload = {
     userId: string;
     type: TokenType;
+    expires?: number,
 };
 
 @Service()
@@ -27,20 +28,24 @@ export default class TokenService {
 
     public async generateAccessToken(user: User, expiresIn: number): Promise<GeneratedToken> {
         const expires = Date.now() + expiresIn;
-        const payload = { userId: user._id.toString(), type: TokenType.ACCESS };
-        const token = await this.tokenStrategy.encodeToken<TokenPayload>(payload, expires);
+        const payload = { userId: user._id.toString(), expires, type: TokenType.ACCESS };
+        const token = await this.tokenStrategy.encodeToken<TokenPayload>(payload);
 
         return { token, expires };
-    };
+    }
+
+    public async verifyAccessToken(encoded: string): Promise<TokenPayload | null> {
+        return await this.verifyToken(encoded, TokenType.ACCESS);
+    }
 
     public async generateRefreshToken(user: User, expiresIn: number): Promise<GeneratedToken> {
         const expires = Date.now() + expiresIn;
-        const payload = { userId: user._id.toString(), type: TokenType.REFRESH };
-        const token = await this.tokenStrategy.encodeToken<TokenPayload>(payload, expires);
+        const payload = { userId: user._id.toString(), expires, type: TokenType.REFRESH };
+        const token = await this.tokenStrategy.encodeToken<TokenPayload>(payload);
         await this.saveToken(token, user._id.toString(), expires, TokenType.REFRESH);
 
         return { token, expires };
-    };
+    }
 
     public async saveToken(token: string, userId: string, expires: number, type: TokenType): Promise<Token> {
         const tokenRecord = new Token();
@@ -50,28 +55,31 @@ export default class TokenService {
         tokenRecord.type = type;
 
         return this.entityManager.save<Token>(tokenRecord);
-    };
+    }
 
-    public async verifyToken(token: string, type: string): Promise<Token | null> {
-        const payload: TokenPayload = await this.tokenStrategy.decodeToken<TokenPayload>(token);
+    public async verifyToken(encoded: string, type: TokenType): Promise<TokenPayload | null> {
+        const payload: TokenPayload = await this.tokenStrategy.decodeToken<TokenPayload>(encoded);
 
-        const tokenUserId: string = payload.userId;
-        const tokenType: TokenType = payload.type;
-
-        if (!tokenUserId || !tokenType || tokenType !== type) {
+        if (!payload?.userId || payload?.type !== type || (payload?.expires && payload.expires < Date.now())) {
             return null;
         }
 
-        const tokenRecord: Token = await this.tokenRepository.findOneBy({ token, type, userId: tokenUserId });
+        return payload;
+    }
 
-        if (!tokenRecord.expires) {
-            return tokenRecord;
-        }
+    public async getVerifiedToken(encoded: string, type: TokenType): Promise<Token | null> {
+        const payload: TokenPayload = await this.verifyToken(encoded, type);
 
-        if (tokenRecord.expires < Date.now()) {
+        if (!payload) {
             return null;
         }
 
-        return tokenRecord;
+        const token: Token = await this.tokenRepository.findOneByAttrs(encoded, type, payload.userId);
+
+        if (!token || token?.expires < Date.now()) {
+            return null;
+        }
+
+        return token;
     }
 }
