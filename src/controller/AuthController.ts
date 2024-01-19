4c79ca9901af9fe1fcaf840d6c5e56e60222e760
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-18 22:13:36 -0700

    separate auth controller

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,28 +1,46 @@
-import { JsonController, Post, Body } from "routing-controllers";
+import { JsonController, Post, Body, HttpCode, NotFoundError, ForbiddenError } from "routing-controllers";
 import { Inject, Service } from "typedi";
-import UserTransfer from "../transfer/user/UserTransfer";
-import AuthService, { AuthTokens } from "../service/auth/AuthService";
-import UserCredentialsTransfer from "../transfer/user/UserCredentialsTransfer";
+import AuthService from "../service/auth/AuthService";
+import { OpenAPI, ResponseSchema } from "routing-controllers-openapi";
+import AuthSchema from "../schema/auth/AuthSchema";
+import TokenSchema from "../schema/auth/TokenSchema";
+import UserService from "../service/user/UserService";
 import User from "../entity/User";
+import UserNotFoundError from "../error/user/UserNotFoundError";
+import UserWrongCredentialsError from "../error/user/UserWrongCredentialsError";
 
 @Service()
-@JsonController('/auth')
+@JsonController()
 export default class AuthController {
 
     constructor(
+        @Inject() private readonly userService: UserService,
         @Inject() private readonly authService: AuthService,
     ) {
     }
 
-    @Post('/register')
-    public async register(@Body({ required: true }) transfer: UserTransfer): Promise<AuthTokens> {
-        const user: User = await this.authService.registerUser(transfer);
+    @Post('/auth')
+    @HttpCode(201)
+    @OpenAPI({
+        responses: {
+            201: { description: 'Created' },
+            403: { description: 'Forbidden' },
+            404: { description: 'Not Found' },
+        },
+    })
+    @ResponseSchema(TokenSchema)
+    public async create(@Body({ required: true }) auth: AuthSchema): Promise<TokenSchema> {
+        try {
+            const user: User = await this.userService.getUserByAuth(auth);
 
-        return await this.authService.authorizeUser(user);
-    }
-
-    @Post('/login')
-    public async login(@Body({ required: true }) transfer: UserCredentialsTransfer): Promise<AuthTokens> {
-        return await this.authService.loginUser(transfer);
+            return await this.authService.createAuth(user);
+        } catch (error: any) {
+            switch (true) {
+                case error instanceof UserNotFoundError:
+                    throw new NotFoundError(error.message);
+                case error instanceof UserWrongCredentialsError:
+                    throw new ForbiddenError(error.message);
+            }
+        }
     }
 }
