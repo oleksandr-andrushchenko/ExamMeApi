64514aed1024025b288a5e7d4d580ccb1d14b241
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-18 22:03:53 -0700

    new methods added: find one by email, find one by id

--- src/repository/CategoryRepository.ts
+++ src/repository/CategoryRepository.ts
@@ -1,9 +1,18 @@
 import { MongoRepository } from "typeorm";
 import Category from "../entity/Category";
 import Repository from "../decorator/Repository";
+import { ObjectId } from "mongodb";
 
 @Repository(Category)
 export default class CategoryRepository extends MongoRepository<Category> {
+
+    public async findOneById(id: string): Promise<Category | undefined> {
+        return await this.findOneBy({ _id: new ObjectId(id) });
+    }
+
+    public async findOneByName(name: string): Promise<Category | undefined> {
+        return await this.findOneBy({ name });
+    }
 
     public async findAll(): Promise<Category[]> {
         return await this.find();
