782f40aea99e55200a308e83f54f0d43c771ef49
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-18 22:05:20 -0700

    constructor customized

--- src/error/user/UserNotFoundError.ts
+++ src/error/user/UserNotFoundError.ts
@@ -1,2 +1,6 @@
 export default class UserNotFoundError extends Error {
+
+    constructor(idOrMessage: string = null) {
+        super(idOrMessage ? `User with id="${idOrMessage}" not found error` : idOrMessage);
+    }
 }
