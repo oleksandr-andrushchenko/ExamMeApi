4af7690f14cbc6e8db8f28863b6594ff6fbd155b
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-18 22:13:54 -0700

    basic crud methods added

--- src/controller/CategoryController.ts
+++ src/controller/CategoryController.ts
@@ -1,10 +1,24 @@
-import { JsonController, Get } from "routing-controllers";
+import {
+    JsonController,
+    Get, Post, Delete, Put, Patch,
+    Body,
+    HttpCode,
+    CurrentUser,
+    Param,
+    NotFoundError, ForbiddenError, Authorized
+} from "routing-controllers";
 import { Inject, Service } from "typedi";
 import Category from "../entity/Category";
 import CategoryRepository from "../repository/CategoryRepository";
 import InjectRepository from "../decorator/InjectRepository";
-import LoggerInterface from "../logger/LoggerInterface";
-import InjectEntityManager, { EntityManagerInterface } from "../decorator/InjectEntityManager";
+import { OpenAPI, ResponseSchema } from "routing-controllers-openapi";
+import CategoryService from "../service/category/CategoryService";
+import User from "../entity/User";
+import CategorySchema from "../schema/category/CategorySchema";
+import CategoryNotFoundError from "../error/category/CategoryNotFoundError";
+import CategoryOwnershipError from "../error/category/CategoryOwnershipError";
+import CategoryNameTakenError from "../error/category/CategoryNameTakenError";
+import ConflictHttpError from "../error/http/ConflictHttpError";
 
 @Service()
 @JsonController('/categories')
@@ -12,13 +26,151 @@ export default class CategoryController {
 
     constructor(
         @InjectRepository() private readonly categoryRepository: CategoryRepository,
-        @InjectEntityManager() private readonly entityManager: EntityManagerInterface,
-        @Inject('logger') private readonly logger: LoggerInterface,
+        @Inject() private readonly categoryService: CategoryService,
     ) {
     }
 
-    @Get('/')
-    public getAll(): Promise<Category[]> {
+    @Post()
+    @Authorized()
+    @HttpCode(201)
+    @OpenAPI({
+        security: [{ bearerAuth: [] }],
+        responses: {
+            201: { description: 'Created' },
+            401: { description: 'Unauthorized' },
+            409: { description: 'Conflict' },
+        },
+    })
+    @ResponseSchema(Category)
+    public async create(
+        @CurrentUser({ required: true }) user: User,
+        @Body({ required: true }) category: CategorySchema,
+    ): Promise<Category> {
+        try {
+            return await this.categoryService.createCategory(category, user);
+        } catch (error: any) {
+            switch (true) {
+                case error instanceof CategoryNameTakenError:
+                    throw new ConflictHttpError(error.message);
+            }
+        }
+    }
+
+    @Get()
+    @OpenAPI({
+        responses: {
+            200: { description: 'OK' },
+        },
+    })
+    @ResponseSchema(Category, { isArray: true })
+    public async query(): Promise<Category[]> {
         return this.categoryRepository.findAll();
+    }
+
+    @Get('/:id')
+    @OpenAPI({
+        responses: {
+            200: { description: 'OK' },
+            404: { description: 'Not Found' },
+        },
+    })
+    @ResponseSchema(Category)
+    public async find(@Param('id') id: string): Promise<Category> {
+        try {
+            return await this.categoryService.getCategory(id);
+        } catch (error: any) {
+            switch (true) {
+                case error instanceof CategoryNotFoundError:
+                    throw new NotFoundError(error.message);
+            }
+        }
+    }
+
+    @Put('/:id')
+    @Authorized()
+    @HttpCode(205)
+    @OpenAPI({
+        security: [{ bearerAuth: [] }],
+        responses: {
+            205: { description: 'Reset Content' },
+            401: { description: 'Unauthorized' },
+            403: { description: 'Forbidden' },
+            404: { description: 'Not Found' },
+        },
+    })
+    @ResponseSchema(Category)
+    public async replace(
+        @CurrentUser({ required: true }) user: User,
+        @Param('id') id: string,
+        @Body({ required: true }) category: CategorySchema,
+    ): Promise<Category> {
+        try {
+            return await this.categoryService.replaceCategoryById(id, category, user);
+        } catch (error: any) {
+            switch (true) {
+                case error instanceof CategoryNotFoundError:
+                    throw new NotFoundError(error.message);
+                case error instanceof CategoryOwnershipError:
+                    throw new ForbiddenError(error.message);
+            }
+        }
+    }
+
+    @Patch('/:id')
+    @Authorized()
+    @HttpCode(205)
+    @OpenAPI({
+        security: [{ bearerAuth: [] }],
+        responses: {
+            205: { description: 'Reset Content' },
+            401: { description: 'Unauthorized' },
+            403: { description: 'Forbidden' },
+            404: { description: 'Not Found' },
+        },
+    })
+    @ResponseSchema(Category)
+    public async update(
+        @CurrentUser({ required: true }) user: User,
+        @Param('id') id: string,
+        @Body({ required: true }) category: CategorySchema,
+    ): Promise<Category> {
+        try {
+            return await this.categoryService.updateCategoryById(id, category, user);
+        } catch (error: any) {
+            switch (true) {
+                case error instanceof CategoryNotFoundError:
+                    throw new NotFoundError(error.message);
+                case error instanceof CategoryOwnershipError:
+                    throw new ForbiddenError(error.message);
+            }
+        }
+    }
+
+    @Delete('/:id')
+    @Authorized()
+    @HttpCode(204)
+    @OpenAPI({
+        security: [{ bearerAuth: [] }],
+        responses: {
+            204: { description: 'No Content' },
+            401: { description: 'Unauthorized' },
+            403: { description: 'Forbidden' },
+            404: { description: 'Not Found' },
+        },
+    })
+    public async delete(
+        @CurrentUser() user: User,
+        @Param('id') id: string,
+    ): Promise<void> {
+        try {
+            await this.categoryService.deleteCategoryById(id, user);
+        } catch (error: any) {
+            switch (true) {
+                case error instanceof CategoryOwnershipError:
+                    throw new ForbiddenError(error.message);
+                case error instanceof CategoryNotFoundError:
+                    throw new NotFoundError(error.message);
+            }
+        }
     }
 }
