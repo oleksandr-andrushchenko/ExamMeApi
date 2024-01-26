6edf33219d74943536ae955a4a44f51630990705
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-26 15:34:12 -0700

    function renamed, user object instead of id added

--- src/repository/TokenRepository.ts
+++ src/repository/TokenRepository.ts
@@ -1,11 +1,12 @@
 import { MongoRepository } from "typeorm";
 import Token, { TokenType } from "../entity/Token";
 import Repository from "../decorator/Repository";
+import User from "../entity/User";
 
 @Repository(Token)
 export default class TokenRepository extends MongoRepository<Token> {
 
-    public async findOneByAttrs(token: string, type: TokenType, userId: string): Promise<Token | null> {
-        return await this.findOneBy({ token, type, userId });
+    public async findOneByTokenAndTypeAndUser(token: string, type: TokenType, user: User): Promise<Token | null> {
+        return await this.findOneBy({ token, type, userId: user._id });
     }
 }
