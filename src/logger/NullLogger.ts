727ddbb3c619272a3cb85473eb95a96edc3e68e2
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-26 22:53:17 -0700

    null logger added

---
+++ src/logger/NullLogger.ts
@@ -0,0 +1,9 @@
+import LoggerInterface from "./LoggerInterface";
+import Logger from "./Logger";
+
+export default class NullLogger extends Logger implements LoggerInterface {
+
+    protected log(level: string, message: string, args: unknown[]): LoggerInterface {
+        return this;
+    }
+}
