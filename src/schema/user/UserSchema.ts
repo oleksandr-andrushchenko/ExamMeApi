a86689cde9b5951369bb5f81acd257775b77b7fb
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-28 23:29:14 -0700

    extends from user me schema

--- src/schema/user/UserSchema.ts
+++ src/schema/user/UserSchema.ts
@@ -1,18 +1,8 @@
-import { IsNotEmpty, IsEmail, IsStrongPassword, IsEnum, IsOptional } from "class-validator";
+import { IsEnum, IsOptional } from "class-validator";
 import { Permission } from "../../type/auth/Permission";
+import UserMeSchema from "./UserMeSchema";
 
-export default class UserSchema {
-
-    @IsNotEmpty()
-    public readonly name: string;
-
-    @IsNotEmpty()
-    @IsEmail()
-    public readonly email: string;
-
-    @IsNotEmpty()
-    @IsStrongPassword({ minLength: 5, minLowercase: 0, minNumbers: 0, minSymbols: 0, minUppercase: 0 })
-    public readonly password: string;
+export default class UserSchema extends UserMeSchema {
 
     @IsOptional()
     @IsEnum(Permission, { each: true })
