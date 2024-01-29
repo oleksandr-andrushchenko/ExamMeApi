a1d87ac6e5f37a4f0ec7e5dfc192cb2f85d326f9
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-28 23:28:43 -0700

    user register schema added

---
+++ src/schema/user/UserMeSchema.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty, IsEmail, IsStrongPassword, IsEnum, IsOptional } from "class-validator";
+import { Permission } from "../../type/auth/Permission";
+
+export default class UserMeSchema {
+
+    @IsNotEmpty()
+    public readonly name: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    public readonly email: string;
+
+    @IsNotEmpty()
+    @IsStrongPassword({ minLength: 5, minLowercase: 0, minNumbers: 0, minSymbols: 0, minUppercase: 0 })
+    public readonly password: string;
+}
