4d5f38b52328deb3093dc2197238aa731806a0bd
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-01-28 21:55:37 -0700

    find one by name method: ignore id param added

--- src/repository/CategoryRepository.ts
+++ src/repository/CategoryRepository.ts
@@ -10,8 +10,14 @@ export default class CategoryRepository extends MongoRepository<Category> {
         return await this.findOneBy({ _id: new ObjectId(id) });
     }
 
-    public async findOneByName(name: string): Promise<Category | undefined> {
-        return await this.findOneBy({ name });
+    public async findOneByName(name: string, ignoreId: ObjectId = undefined): Promise<Category | undefined> {
+        const where = { name };
+
+        if (ignoreId) {
+            where['_id'] = { $ne: ignoreId };
+        }
+
+        return await this.findOneBy(where);
     }
 
     public async findAll(): Promise<Category[]> {
