1fdb36c17a3c51e64cf679432d058ebcfb8f5fe7
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-02 15:42:51 -0700

    replace me endpoint added

--- src/controller/MeController.ts
+++ src/controller/MeController.ts
@@ -2,7 +2,7 @@ import {
     JsonController,
     Post,
     Body,
-    HttpCode, Get, Authorized, CurrentUser,
+    HttpCode, Get, Authorized, CurrentUser, Put, OnUndefined,
 } from "routing-controllers";
 import { Inject, Service } from "typedi";
 import User from "../entity/User";
@@ -11,6 +11,7 @@ import UserEmailTakenError from "../error/user/UserEmailTakenError";
 import ConflictHttpError from "../error/http/ConflictHttpError";
 import MeSchema from "../schema/user/MeSchema";
 import MeService from "../service/user/MeService";
+import Category from "../entity/Category";
 
 @Service()
 @JsonController('/me')
@@ -57,5 +58,33 @@ export default class UserController {
         @CurrentUser({ required: true }) user: User,
     ): Promise<User> {
         return user;
+    }
+
+    @Put()
+    @Authorized()
+    @HttpCode(205)
+    @OnUndefined(205)
+    @OpenAPI({
+        security: [{ bearerAuth: [] }],
+        responses: {
+            205: { description: 'Reset Content' },
+            400: { description: 'Bad Request' },
+            401: { description: 'Unauthorized' },
+            409: { description: 'Conflict' },
+        },
+    })
+    @ResponseSchema(Category)
+    public async replaceMe(
+        @CurrentUser({ required: true }) user: User,
+        @Body({ required: true }) me: MeSchema,
+    ): Promise<void> {
+        try {
+            await this.meService.replaceMe(me, user);
+        } catch (error) {
+            switch (true) {
+                case error instanceof UserEmailTakenError:
+                    throw new ConflictHttpError((error as UserEmailTakenError).message);
+            }
+        }
     }
 }
