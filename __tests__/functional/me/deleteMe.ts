bdfe70dc7f59cb5dc3cfc2989a75385d32570bda
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-02 16:21:21 -0700

    functional tests for delete me endpoint added

---
+++ __tests__/functional/me/deleteMe.ts
@@ -0,0 +1,26 @@
+import { describe, expect, test } from '@jest/globals';
+import request from "supertest";
+// @ts-ignore
+import { api, fixture, error, auth, load } from "../../index";
+import User from "../../../src/entity/User";
+
+describe('DELETE /me', () => {
+    const app = api();
+
+    test('Unauthorized', async () => {
+        const res = await request(app).delete(`/me`);
+
+        expect(res.status).toEqual(401);
+        expect(res.body).toMatchObject(error('AuthorizationRequiredError'));
+    });
+
+    test('Deleted', async () => {
+        const user = await fixture<User>(User);
+        const token = (await auth(user)).token;
+        const res = await request(app).delete(`/me`).auth(token, { type: 'bearer' });
+
+        expect(res.status).toEqual(204);
+        expect(res.body).toEqual({});
+        expect(await load<User>(User, user.getId())).toBeNull();
+    });
+});
