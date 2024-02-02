8639ce0312f9ee8c917df574de31e7a3684eb186
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-02 13:38:09 -0700

    functional tests for user creation

---
+++ __tests__/functional/user/createUser.ts
@@ -0,0 +1,67 @@
+import { describe, expect, test } from '@jest/globals';
+import request from "supertest";
+// @ts-ignore
+import { api, fixture, error, auth, load } from "../../index";
+import User from "../../../src/entity/User";
+import { Permission } from "../../../src/type/auth/Permission";
+import { ObjectId } from "mongodb";
+
+describe('POST /users', () => {
+    const app = api();
+
+    test('Unauthorized', async () => {
+        const res = await request(app).post('/users');
+
+        expect(res.status).toEqual(401);
+        expect(res.body).toMatchObject(error('AuthorizationRequiredError'));
+    });
+
+    test('Bad request', async () => {
+        const user = await fixture<User>(User, { permissions: [Permission.CREATE_USER] });
+        const token = (await auth(user)).token;
+        const res = await request(app).post('/users').send({}).auth(token, { type: 'bearer' });
+
+        expect(res.status).toEqual(400);
+        expect(res.body).toMatchObject(error('BadRequestError'));
+    });
+
+    test('Forbidden', async () => {
+        const user = await fixture<User>(User, { permissions: [Permission.REGULAR] });
+        const token = (await auth(user)).token;
+        const res = await request(app).post('/users').send({
+            name: 'any',
+            email: 'a@a.com',
+            password: '123123'
+        }).auth(token, { type: 'bearer' });
+
+        expect(res.status).toEqual(403);
+        expect(res.body).toMatchObject(error('ForbiddenError'));
+    });
+
+    test('Conflict', async () => {
+        const user1 = await fixture<User>(User);
+        const user = await fixture<User>(User, { permissions: [Permission.CREATE_USER] });
+        const token = (await auth(user)).token;
+        const res = await request(app).post('/users').send({
+            name: 'any',
+            email: user1.getEmail(),
+            password: '123123'
+        }).auth(token, { type: 'bearer' });
+
+        expect(res.status).toEqual(409);
+        expect(res.body).toMatchObject(error('ConflictError'));
+    });
+
+    test('Created', async () => {
+        const user = await fixture<User>(User, { permissions: [Permission.CREATE_USER] });
+        const token = (await auth(user)).token;
+        const schema = { name: 'any', email: 'a@a.com' };
+        const res = await request(app).post('/users').send({ ...schema, ...{ password: '123123' } }).auth(token, { type: 'bearer' });
+
+        expect(res.status).toEqual(201);
+        expect(res.body).toHaveProperty('id');
+        const id = new ObjectId(res.body.id);
+        expect(res.body).toMatchObject(schema);
+        expect(await load<User>(User, id)).toMatchObject(schema);
+    });
+});
