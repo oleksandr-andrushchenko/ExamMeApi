269c80fcfbd1d9c19618a144ffe5cebf1d831621
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-02-03 00:01:54 -0700

    errors json appended to message

--- src/error/validator/ValidatorError.ts
+++ src/error/validator/ValidatorError.ts
@@ -5,7 +5,7 @@ export default class ValidatorError extends Error {
     constructor(
         private readonly errors: ValidationError[],
     ) {
-        super(`Validation error${errors.length === 1 ? '' : 's'}`);
+        super(`Validation error${errors.length === 1 ? '' : 's'} ${JSON.stringify(errors)}`);
     }
 
     public getErrors(): ValidationError[] {
