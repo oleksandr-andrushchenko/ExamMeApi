7686cf499e14484fccace715b7f8e203414e5946
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-07 01:05:14 -0700

    question count column added

--- src/entity/Category.ts
+++ src/entity/Category.ts
@@ -1,7 +1,7 @@
-import { Column, CreateDateColumn, DeleteDateColumn, Entity, ObjectIdColumn, UpdateDateColumn, } from 'typeorm'
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, ObjectIdColumn, UpdateDateColumn } from 'typeorm'
 import { Exclude, Transform } from 'class-transformer'
 import { ObjectId } from 'mongodb'
-import { IsMongoId, IsNumber, IsOptional, Length } from 'class-validator'
+import { IsMongoId, IsNumber, IsOptional, Length, Min } from 'class-validator'
 
 @Entity({ name: 'categories' })
 export default class Category {
@@ -14,6 +14,10 @@ export default class Category {
   @Length(3, 30)
   @Column({ unique: true, nullable: false })
   private name: string
+
+  @Min(0)
+  @Column({ default: 0, nullable: false })
+  private questionCount: number = 0
 
   @Exclude()
   @IsMongoId()
@@ -54,6 +58,16 @@ export default class Category {
 
   public getName(): string {
     return this.name
+  }
+
+  public setQuestionCount(count: number): this {
+    this.questionCount = count
+
+    return this
+  }
+
+  public getQuestionCount(): number {
+    return this.questionCount
   }
 
   public setCreator(creator: ObjectId): this {
