6b04a23071f9eccbfb9c8338ae04c6dd47f31857
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-20 11:18:18 -0600

    non-func refactoring

--- src/schema/pagination/PaginationSchema.ts
+++ src/schema/pagination/PaginationSchema.ts
@@ -1,16 +1,6 @@
-import { IsIn, IsMongoId, IsNumber, IsOptional, IsPositive, IsString, Max } from 'class-validator'
+import { IsIn, IsMongoId, IsNumber, IsOptional, IsPositive, IsString, Max, Min } from 'class-validator'
 
 export default class PaginationSchema {
-
-  @IsOptional()
-  @IsNumber()
-  @IsPositive()
-  @Max(50)
-  public readonly size: number = 1
-
-  @IsOptional()
-  @IsIn([ 'asc', 'desc' ])
-  public readonly order: 'asc' | 'desc' = 'desc'
 
   @IsOptional()
   @IsString()
@@ -25,4 +15,15 @@ export default class PaginationSchema {
   @IsOptional()
   @IsIn([ '_id', 'created', 'updated' ])
   public readonly cursor: string = '_id'
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  @Max(50)
+  public readonly size: number = 1
+
+  @IsOptional()
+  @IsIn([ 'asc', 'desc' ])
+  public readonly order: 'asc' | 'desc' = 'desc'
 }
