e440b5f1ea707f7e4ba361827e10fb9f87513ffb
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-03-20 21:34:07 -0600

    query categories tests updated: cursor cases added

--- __tests__/functional/category/queryCategories.ts
+++ __tests__/functional/category/queryCategories.ts
@@ -3,6 +3,7 @@ import request from 'supertest'
 // @ts-ignore
 import { api, fixture } from '../../index'
 import Category from '../../../src/entity/Category'
+import * as querystring from 'querystring'
 
 describe('GET /categories', () => {
   const app = api()
@@ -12,6 +13,118 @@ describe('GET /categories', () => {
 
     expect(res.status).toEqual(200)
     expect(res.body.data).toEqual([])
+  })
+
+  test('Cursor (id, id:asc)', async () => {
+    const categories = (await Promise.all([ fixture<Category>(Category), fixture<Category>(Category), fixture<Category>(Category) ]))
+      .sort((a: Category, b: Category) => a.getId().toString().localeCompare(b.getId().toString()))
+
+    const query = { cursor: 'id', size: 1, order: 'asc' }
+    const res = await request(app).get('/categories').query(query)
+
+    expect(res.status).toEqual(200)
+    expect(res.body.data).toHaveLength(query.size)
+    const lastInBodyId = res.body.data[query.size - 1].id
+    const lastInStorageId = categories[query.size - 1].getId().toString()
+    expect(lastInBodyId).toEqual(lastInStorageId)
+    expect(res.body.meta).toMatchObject(query)
+    expect(res.body.meta).toMatchObject({ nextCursor: lastInStorageId })
+    expect(res.body.meta).toMatchObject({ nextUrl: '?' + querystring.stringify({ ...query, ...{ nextCursor: lastInStorageId } }) })
+  })
+
+  test('Cursor (id, id:desc)', async () => {
+    const categories = (await Promise.all([ fixture<Category>(Category), fixture<Category>(Category), fixture<Category>(Category) ]))
+      .sort((a: Category, b: Category) => a.getId().toString().localeCompare(b.getId().toString()))
+
+    const size = 2
+    const query = { cursor: 'id', size, order: 'desc' }
+    const res = await request(app).get('/categories').query(query)
+
+    expect(res.status).toEqual(200)
+    expect(res.body.data).toHaveLength(query.size)
+    const firstInBodyId = res.body.data[0].id
+    const lastInStorageId = categories[categories.length - 1].getId().toString()
+    expect(firstInBodyId).toEqual(lastInStorageId)
+    const firstInStorage = categories[categories.length - size].getId().toString()
+    const lastInBodyId = res.body.data[size - 1].id
+    expect(lastInBodyId).toEqual(firstInStorage)
+    expect(res.body.meta).toMatchObject(query)
+    expect(res.body.meta).toMatchObject({ nextCursor: firstInStorage })
+    expect(res.body.meta).toMatchObject({ nextUrl: '?' + querystring.stringify({ ...query, ...{ nextCursor: firstInStorage } }) })
+  })
+
+  test('Cursor (id, id:asc, prev)', async () => {
+    const categories = (await Promise.all([ fixture<Category>(Category), fixture<Category>(Category), fixture<Category>(Category) ]))
+      .sort((a: Category, b: Category) => a.getId().toString().localeCompare(b.getId().toString()))
+
+    const firstInStorageId = categories[0].getId().toString()
+    const secondInStorageId = categories[1].getId().toString()
+    const query = { cursor: 'id', size: 1, order: 'asc' }
+    const res = await request(app).get('/categories').query({ ...query, ...{ prevCursor: secondInStorageId } })
+
+    expect(res.status).toEqual(200)
+    expect(res.body.data).toHaveLength(query.size)
+    const firstInBodyId = res.body.data[0].id
+    expect(firstInBodyId).toEqual(firstInStorageId)
+    expect(res.body.meta).toMatchObject({ ...query, ...{ nextCursor: firstInStorageId } })
+  })
+
+  test('Cursor (id, id:asc, next)', async () => {
+    const categories = (await Promise.all([ fixture<Category>(Category), fixture<Category>(Category), fixture<Category>(Category) ]))
+      .sort((a: Category, b: Category) => a.getId().toString().localeCompare(b.getId().toString()))
+
+    const firstInStorageId = categories[0].getId().toString()
+    const secondInStorageId = categories[1].getId().toString()
+    const query = { cursor: 'id', size: 1, order: 'asc' }
+    const res = await request(app).get('/categories').query({ ...query, ...{ nextCursor: firstInStorageId } })
+
+    expect(res.status).toEqual(200)
+    expect(res.body.data).toHaveLength(query.size)
+    const firstInBodyId = res.body.data[0].id
+    expect(firstInBodyId).toEqual(secondInStorageId)
+    expect(res.body.meta).toMatchObject({
+      ...query, ...{
+        prevCursor: secondInStorageId,
+        nextCursor: secondInStorageId,
+      },
+    })
+  })
+
+  test('Cursor (id, id:desc, prev)', async () => {
+    const categories = (await Promise.all([ fixture<Category>(Category), fixture<Category>(Category), fixture<Category>(Category) ]))
+      .sort((a: Category, b: Category) => a.getId().toString().localeCompare(b.getId().toString()))
+
+    const secondInStorageId = categories[1].getId().toString()
+    const lastInStorageId = categories[categories.length - 1].getId().toString()
+    const query = { cursor: 'id', size: 1, order: 'desc' }
+    const res = await request(app).get('/categories').query({ ...query, ...{ prevCursor: secondInStorageId } })
+
+    expect(res.status).toEqual(200)
+    expect(res.body.data).toHaveLength(query.size)
+    const firstInBodyId = res.body.data[0].id
+    expect(firstInBodyId).toEqual(lastInStorageId)
+    expect(res.body.meta).toMatchObject({ ...query, ...{ nextCursor: firstInBodyId } })
+  })
+
+  test('Cursor (id, id:desc, next)', async () => {
+    const categories = (await Promise.all([ fixture<Category>(Category), fixture<Category>(Category), fixture<Category>(Category) ]))
+      .sort((a: Category, b: Category) => a.getId().toString().localeCompare(b.getId().toString()))
+
+    const secondInStorageId = categories[1].getId().toString()
+    const lastInStorageId = categories[categories.length - 1].getId().toString()
+    const query = { cursor: 'id', size: 1, order: 'desc' }
+    const res = await request(app).get('/categories').query({ ...query, ...{ nextCursor: lastInStorageId } })
+
+    expect(res.status).toEqual(200)
+    expect(res.body.data).toHaveLength(query.size)
+    const firstInBodyId = res.body.data[0].id
+    expect(firstInBodyId).toEqual(secondInStorageId)
+    expect(res.body.meta).toMatchObject({
+      ...query, ...{
+        prevCursor: secondInStorageId,
+        nextCursor: secondInStorageId,
+      },
+    })
   })
 
   test('Not empty', async () => {
