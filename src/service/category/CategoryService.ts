763e62cfc1736982995b92ee8c35b24d3e14320d
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-03 22:22:00 -0600

    methods signatures updated

--- src/service/category/CategoryService.ts
+++ src/service/category/CategoryService.ts
@@ -56,12 +56,16 @@ export default class CategoryService {
   }
 
   /**
-   * @param {string} id
+   * @param {ObjectId | string} id
    * @returns {Promise<Category>}
    * @throws {CategoryNotFoundError}
    */
-  public async getCategory(id: string): Promise<Category> {
-    this.validator.validateId(id)
+  public async getCategory(id: ObjectId | string): Promise<Category> {
+    if (typeof id === 'string') {
+      this.validator.validateId(id)
+      id = new ObjectId(id)
+    }
+
     const category: Category = await this.categoryRepository.findOneById(id)
 
     if (!category) {
@@ -72,11 +76,16 @@ export default class CategoryService {
   }
 
   /**
+   *
    * @param {PaginationSchema} pagination
-   * @returns {Promise<PaginatedSchema<Category>>}
+   * @param {boolean} meta
+   * @returns {Promise<Category[] | PaginatedSchema<Category>>}
    * @throws {ValidatorError}
    */
-  public async queryCategories(pagination: PaginationSchema): Promise<PaginatedSchema<Category>> {
+  public async queryCategories(
+    pagination: PaginationSchema,
+    meta: boolean = false,
+  ): Promise<Category[] | PaginatedSchema<Category>> {
     await this.validator.validate(pagination)
 
     const cursor = new Cursor<Category>(pagination, this.categoryRepository)
@@ -93,11 +102,11 @@ export default class CategoryService {
       }
     }
 
-    return await cursor.getPaginated(where)
+    return await cursor.getPaginated(where, meta)
   }
 
   /**
-   * @param {string} id
+   * @param {Category} category
    * @param {CategoryUpdateSchema} transfer
    * @param {User} initiator
    * @returns {Promise<Category>}
@@ -105,10 +114,7 @@ export default class CategoryService {
    * @throws {AuthorizationFailedError}
    * @throws {CategoryNameTakenError}
    */
-  public async updateCategory(id: string, transfer: CategoryUpdateSchema, initiator: User): Promise<Category> {
-    this.validator.validateId(id)
-    const category: Category = await this.getCategory(id)
-
+  public async updateCategory(category: Category, transfer: CategoryUpdateSchema, initiator: User): Promise<Category> {
     await this.authService.verifyAuthorization(initiator, Permission.UPDATE_CATEGORY, category)
 
     await this.validator.validate(transfer)
@@ -128,7 +134,7 @@ export default class CategoryService {
   }
 
   /**
-   * @param {string} id
+   * @param {Category} category
    * @param {CategorySchema} transfer
    * @param {User} initiator
    * @returns {Promise<Category>}
@@ -136,10 +142,7 @@ export default class CategoryService {
    * @throws {AuthorizationFailedError}
    * @throws {CategoryNameTakenError}
    */
-  public async replaceCategory(id: string, transfer: CategorySchema, initiator: User): Promise<Category> {
-    this.validator.validateId(id)
-    const category: Category = await this.getCategory(id)
-
+  public async replaceCategory(category: Category, transfer: CategorySchema, initiator: User): Promise<Category> {
     await this.authService.verifyAuthorization(initiator, Permission.REPLACE_CATEGORY, category)
 
     await this.validator.validate(transfer)
@@ -156,16 +159,13 @@ export default class CategoryService {
   }
 
   /**
-   * @param {string} id
+   * @param {Category} category
    * @param {User} initiator
    * @returns {Promise<Category>}
    * @throws {CategoryNotFoundError}
    * @throws {AuthorizationFailedError}
    */
-  public async deleteCategory(id: string, initiator: User): Promise<Category> {
-    this.validator.validateId(id)
-    const category: Category = await this.getCategory(id)
-
+  public async deleteCategory(category: Category, initiator: User): Promise<Category> {
     await this.authService.verifyAuthorization(initiator, Permission.DELETE_CATEGORY, category)
 
     // todo: soft delete
