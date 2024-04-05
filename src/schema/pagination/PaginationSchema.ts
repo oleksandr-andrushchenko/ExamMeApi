d296e7efa713636b4c1a0ad9dd8ac38ffce12101
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-04 21:50:31 -0600

    optional stuff removed

--- src/schema/pagination/PaginationSchema.ts
+++ src/schema/pagination/PaginationSchema.ts
@@ -5,25 +5,25 @@ export default class PaginationSchema {
   @IsOptional()
   @IsString()
   @IsMongoId()
-  public readonly prevCursor?: string
+  public readonly prevCursor: string
 
   @IsOptional()
   @IsString()
   @IsMongoId()
-  public readonly nextCursor?: string
+  public readonly nextCursor: string
 
   @IsOptional()
   @IsIn([ 'id', 'created', 'updated' ])
-  public readonly cursor?: string = 'id'
+  public readonly cursor: string = 'id'
 
   @IsOptional()
   @IsNumber()
   @IsPositive()
   @Min(1)
   @Max(50)
-  public readonly size?: number = 10
+  public readonly size: number = 10
 
   @IsOptional()
   @IsIn([ 'asc', 'desc' ])
-  public readonly order?: 'asc' | 'desc' = 'desc'
+  public readonly order: 'asc' | 'desc' = 'desc'
 }
