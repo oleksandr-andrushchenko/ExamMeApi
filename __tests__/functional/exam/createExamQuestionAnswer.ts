8e6dc3d15f36077e1fc05fcfe2d83a9416c4979b
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-07 00:15:40 -0600

    create exam question answer endpoint functional tests aded

---
+++ __tests__/functional/exam/createExamQuestionAnswer.ts
@@ -0,0 +1,85 @@
+import { describe, expect, test } from '@jest/globals'
+import request from 'supertest'
+// @ts-ignore
+import { api, auth, error, fixture, load } from '../../index'
+import Exam from '../../../src/entity/Exam'
+import Question, { QuestionType } from '../../../src/entity/Question'
+import User from '../../../src/entity/User'
+
+describe('POST /exams/:examId/questions/:question/answer', () => {
+  const app = api()
+
+  test('Unauthorized', async () => {
+    const exam = await fixture<Exam>(Exam)
+    const id = exam.getId()
+    const questionNumber = 0
+    const question = await load<Question>(Question, exam.getQuestions()[questionNumber].getQuestion())
+    const body = {}
+
+    if (question.getType() === QuestionType.CHOICE) {
+      body['choice'] = 0
+    } else if (question.getType() === QuestionType.TYPE) {
+      body['answer'] = 'any'
+    }
+
+    const res = await request(app).post(`/exams/${ id.toString() }/questions/${ questionNumber }/answer`).send(body)
+
+    expect(res.status).toEqual(401)
+    expect(res.body).toMatchObject(error('AuthorizationRequiredError'))
+  })
+
+  test('Bad request (empty body)', async () => {
+    const user = await fixture<User>(User)
+    const token = (await auth(user)).token
+    const exam = await fixture<Exam>(Exam)
+    const id = exam.getId()
+    const questionNumber = 0
+    const res = await request(app).post(`/exams/${ id.toString() }/questions/${ questionNumber }/answer`).auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(400)
+    expect(res.body).toMatchObject(error('BadRequestError'))
+  })
+
+  test('Forbidden', async () => {
+    const user = await fixture<User>(User)
+    const token = (await auth(user)).token
+    const exam = await fixture<Exam>(Exam)
+    const id = exam.getId()
+    const questionNumber = 0
+    const question = await load<Question>(Question, exam.getQuestions()[questionNumber].getQuestion())
+    const body = {}
+
+    if (question.getType() === QuestionType.CHOICE) {
+      body['choice'] = 0
+    } else if (question.getType() === QuestionType.TYPE) {
+      body['answer'] = 'any'
+    }
+
+    const res = await request(app).post(`/exams/${ id.toString() }/questions/${ questionNumber }/answer`).send(body).auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(403)
+    expect(res.body).toMatchObject(error('ForbiddenError'))
+  })
+
+  test('Created', async () => {
+    const exam = await fixture<Exam>(Exam)
+    const id = exam.getId()
+    const user = await load<User>(User, exam.getOwner())
+    const token = (await auth(user)).token
+    const questionNumber = 0
+    const question = await load<Question>(Question, exam.getQuestions()[questionNumber].getQuestion())
+    const body = {}
+
+    if (question.getType() === QuestionType.CHOICE) {
+      body['choice'] = 0
+    } else if (question.getType() === QuestionType.TYPE) {
+      body['answer'] = 'any'
+    }
+
+    const res = await request(app).post(`/exams/${ id.toString() }/questions/${ questionNumber }/answer`).send(body).auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(201)
+    expect(res.body).toHaveProperty('type')
+    expect(res.body).toHaveProperty('difficulty')
+  })
+})
