a299beb87b50b74d97a2b459f7ca6b52e0046ffb
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-07 16:11:31 -0600

    required score property added

--- src/schema/category/CategoryUpdateSchema.ts
+++ src/schema/category/CategoryUpdateSchema.ts
@@ -1,8 +1,14 @@
-import { IsOptional, Length } from 'class-validator'
+import { IsNumber, IsOptional, Length, Max, Min } from 'class-validator'
 
 export default class CategoryUpdateSchema {
 
   @IsOptional()
   @Length(3, 30)
   public readonly name: string
+
+  @IsOptional()
+  @Min(0)
+  @Max(100)
+  @IsNumber({ maxDecimalPlaces: 0 })
+  public readonly requiredScore: number = 0
 }
