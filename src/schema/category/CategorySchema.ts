051789be4577f0be2519aceec16ff5fe55162a74
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-07 16:10:34 -0600

    required score property added

--- src/schema/category/CategorySchema.ts
+++ src/schema/category/CategorySchema.ts
@@ -1,7 +1,12 @@
-import { Length } from 'class-validator'
+import { IsNumber, Length, Max, Min } from 'class-validator'
 
 export default class CategorySchema {
 
   @Length(3, 100)
   public readonly name: string
+
+  @Min(0)
+  @Max(100)
+  @IsNumber({ maxDecimalPlaces: 0 })
+  public readonly requiredScore: number = 0
 }
