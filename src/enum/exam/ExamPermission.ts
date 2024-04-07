d46617e700c7198774eb57f0b61303a0f382f33c
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-07 01:04:57 -0600

    separate exam permissions

---
+++ src/enum/exam/ExamPermission.ts
@@ -0,0 +1,9 @@
+enum ExamPermission {
+  CREATE = 'createExam',
+  GET = 'getExam',
+  GET_QUESTION = 'getExamQuestion',
+  CREATE_QUESTION_ANSWER = 'createExamQuestionAnswer',
+  DELETE = 'deleteExam',
+}
+
+export default ExamPermission
