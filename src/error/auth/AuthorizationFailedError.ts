5c22af244dc941ec94787e035ca7e9a74d77c4ae
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-08 22:00:35 -0600

    permission as string

--- src/error/auth/AuthorizationFailedError.ts
+++ src/error/auth/AuthorizationFailedError.ts
@@ -2,7 +2,7 @@ import Permission from '../../enum/Permission'
 
 export default class AuthorizationFailedError extends Error {
 
-  constructor(permission: Permission) {
+  constructor(permission: string) {
     super(`Permission with id="${ permission }" authorization error`)
   }
 }
