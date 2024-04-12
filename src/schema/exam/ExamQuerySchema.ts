a22a77a4a3d6e8bb54dab3ec47b2dd377e2350ef
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-11 20:59:28 -0600

    completion param added

--- src/schema/exam/ExamQuerySchema.ts
+++ src/schema/exam/ExamQuerySchema.ts
@@ -1,9 +1,13 @@
 import PaginationSchema from '../pagination/PaginationSchema'
-import { IsMongoId, IsOptional } from 'class-validator'
+import { IsBoolean, IsMongoId, IsOptional } from 'class-validator'
 
 export default class ExamQuerySchema extends PaginationSchema {
 
   @IsOptional()
   @IsMongoId()
   public readonly category: string
+
+  @IsOptional()
+  @IsBoolean()
+  public readonly completion: boolean
 }
