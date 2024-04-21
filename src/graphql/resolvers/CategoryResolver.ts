908995daca6e3239e027ebdde039b4c9d76b42c9
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-20 23:42:25 -0600

    category graphql resolver added

---
+++ src/graphql/resolvers/CategoryResolver.ts
@@ -0,0 +1,26 @@
+import { Inject, Service } from 'typedi'
+import CategoryService from '../../service/category/CategoryService'
+import { Arg, Args, Query, Resolver } from 'type-graphql'
+import Category from '../../entity/Category'
+import CategoryQuerySchema from '../../schema/category/CategoryQuerySchema'
+import PaginatedSchema from '../../schema/pagination/PaginatedSchema'
+
+@Service()
+@Resolver(Category)
+export default class CategoryResolver {
+
+  public constructor(
+    @Inject() private readonly categoryService: CategoryService,
+  ) {
+  }
+
+  @Query(_returns => Category)
+  public async category(@Arg('id') id: string): Promise<Category> {
+    return await this.categoryService.getCategory(id)
+  }
+
+  @Query(_returns => [ Category ])
+  public async categories(@Args() query: CategoryQuerySchema): Promise<Category[] | PaginatedSchema<Category>> {
+    return await this.categoryService.queryCategories(query)
+  }
+}
