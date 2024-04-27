d97f0974dfd7ff0a50912167d77e47f6fba69037
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-27 14:12:28 -0600

    soft non-func refactoring

--- src/error/validator/ValidatorError.ts
+++ src/error/validator/ValidatorError.ts
@@ -2,13 +2,7 @@ import { ValidationError } from 'class-validator'
 
 export default class ValidatorError extends Error {
 
-  constructor(
-    private readonly errors: ValidationError[],
-  ) {
+  public constructor(private readonly errors: ValidationError[]) {
     super(`Validation errors ${ JSON.stringify(errors) }`)
-  }
-
-  public getErrors(): ValidationError[] {
-    return this.errors
   }
 }
