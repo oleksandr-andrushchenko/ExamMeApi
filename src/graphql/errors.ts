223ca8568e02223fdb09b78bf2e0619cbe7c7646
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-27 14:11:14 -0600

    graphql errors map added

---
+++ src/graphql/errors.ts
@@ -0,0 +1,7 @@
+import { ApolloServerErrorCode } from '@apollo/server/errors'
+
+export const errors = {
+  'ValidatorError': 'BadRequestError',
+  'CategoryNotFoundError': 'NotFoundError',
+  [ApolloServerErrorCode.BAD_USER_INPUT]: 'BadRequestError',
+}
