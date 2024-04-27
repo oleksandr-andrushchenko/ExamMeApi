f54143e2a85d2d4187a02072595d2761c780866e
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-27 14:14:12 -0600

    graphql get category tests added

---
+++ __tests__/functional/category/getGraphqlCategory.ts
@@ -0,0 +1,83 @@
+import { describe, expect, test } from '@jest/globals'
+import request from 'supertest'
+// @ts-ignore
+import { api, fakeId, fixture, graphqlError } from '../../index'
+import Category from '../../../src/entity/Category'
+
+describe('POST /graphql category', () => {
+  const app = api()
+
+  test('Not found', async () => {
+    const id = await fakeId()
+    const res = await request(app)
+      .post(`/graphql`)
+      .send({
+        query: `query Category($categoryId: String!) {
+          category(id: $categoryId) {
+            id
+          }
+        }`,
+        variables: {
+          categoryId: id.toString(),
+        },
+      })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject(graphqlError('NotFoundError'))
+  })
+
+  test('Bad request (invalid id)', async () => {
+    const id = 'invalid'
+    const res = await request(app)
+      .post(`/graphql`)
+      .send({
+        query: `query Category($categoryId: String!) {
+          category(id: $categoryId) {
+            id
+          }
+        }`,
+        variables: {
+          categoryId: id.toString(),
+        },
+      })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject(graphqlError('BadRequestError'))
+  })
+
+  test('Found', async () => {
+    const category = await fixture<Category>(Category)
+    const id = category.getId()
+    const res = await request(app)
+      .post(`/graphql`)
+      .send({
+        query: `query Category($categoryId: String!) {
+          category(id: $categoryId) {
+            id
+            name
+            questionCount
+            requiredScore
+            voters
+            rating
+          }
+        }`,
+        variables: {
+          categoryId: id.toString(),
+        },
+      })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toEqual({
+      data: {
+        category: {
+          id: id.toString(),
+          name: category.getName(),
+          questionCount: category.getQuestionCount(),
+          requiredScore: category.getRequiredScore(),
+          voters: category.getVoters(),
+          rating: category.getRating(),
+        },
+      },
+    })
+  })
+})
