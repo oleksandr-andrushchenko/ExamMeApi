1f8a0104ed3887621459dd88959c64efa9a02f67
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-28 01:55:13 -0600

    auth graphql resolver added

---
+++ src/graphql/resolvers/AuthResolver.ts
@@ -0,0 +1,25 @@
+import { Inject, Service } from 'typedi'
+import { Arg, Mutation, Resolver } from 'type-graphql'
+import UserService from '../../service/user/UserService'
+import AuthService from '../../service/auth/AuthService'
+import { CredentialsSchema } from '../../schema/auth/CredentialsSchema'
+import TokenSchema from '../../schema/auth/TokenSchema'
+
+@Service()
+@Resolver()
+export class AuthResolver {
+
+  public constructor(
+    @Inject() private readonly userService: UserService,
+    @Inject() private readonly authService: AuthService,
+  ) {
+  }
+
+  @Mutation(_returns => TokenSchema)
+  public async authenticate(@Arg('credentials') credentials: CredentialsSchema): Promise<TokenSchema> {
+
+    const user = await this.userService.getUserByCredentials(credentials)
+
+    return await this.authService.createAuth(user)
+  }
+}
