c8a228a411b553d68d3651c3976364d7dd73b5f8
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-28 01:26:18 -0600

    graphql decorators added

--- src/schema/auth/TokenSchema.ts
+++ src/schema/auth/TokenSchema.ts
@@ -1,8 +1,11 @@
 import { IsNumber, IsString } from 'class-validator'
+import { Field, Int, ObjectType } from 'type-graphql'
 
+@ObjectType()
 export default class TokenSchema {
 
   @IsString()
+  @Field()
   public token: string
 
   @IsNumber()
--- src/schema/category/CategorySchema.ts
+++ src/schema/category/CategorySchema.ts
@@ -1,12 +1,16 @@
 import { IsNumber, Length, Max, Min } from 'class-validator'
+import { Field, InputType, Int } from 'type-graphql'
 
+@InputType()
 export default class CategorySchema {
 
   @Length(3, 100)
+  @Field()
   public readonly name: string
 
   @Min(0)
   @Max(100)
   @IsNumber({ maxDecimalPlaces: 0 })
+  @Field(_type => Int, { defaultValue: 0 })
   public readonly requiredScore: number = 0
 }
