804f2e969886987f9765f551afe20c76148354f5
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-27 23:30:34 -0600

    synced with latest changes

--- __tests__/graphql/category/categoryQuery.ts
+++ __tests__/graphql/category/categoryQuery.ts
@@ -1,8 +1,8 @@
 export const categoryQuery = (fields: string[] = [ 'id' ], variables: object = {}) => {
   return {
     query: `
-      query Category($categoryId: String!) {
-        category(id: $categoryId) {
+      query Category($categoryId: ID!) {
+        category(categoryId: $categoryId) {
           ${ fields.join('\r') }
         }
       }
