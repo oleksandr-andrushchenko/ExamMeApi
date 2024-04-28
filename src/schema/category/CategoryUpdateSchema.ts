f23fb4357dd7e546f3c26d1a4321ed9c8dc59b40
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-28 01:30:38 -0600

    graphql decorators added

--- src/schema/category/CategoryUpdateSchema.ts
+++ src/schema/category/CategoryUpdateSchema.ts
@@ -1,14 +1,18 @@
 import { IsNumber, IsOptional, Length, Max, Min } from 'class-validator'
+import { Field, InputType, Int } from 'type-graphql'
 
+@InputType()
 export default class CategoryUpdateSchema {
 
   @IsOptional()
   @Length(3, 30)
-  public readonly name: string
+  @Field({ nullable: true })
+  public readonly name?: string
 
   @IsOptional()
   @Min(0)
   @Max(100)
   @IsNumber({ maxDecimalPlaces: 0 })
-  public readonly requiredScore: number = 0
+  @Field(_type => Int, { nullable: true })
+  public readonly requiredScore?: number = 0
 }
