3ebc7d1b36fe89c8a5b836fc75b86efa0bb147c2
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-28 01:54:34 -0600

    validator added & implemented; refactoring

--- src/graphql/resolvers/CategoryResolver.ts
+++ src/graphql/resolvers/CategoryResolver.ts
@@ -4,23 +4,31 @@ import { Args, Query, Resolver } from 'type-graphql'
 import Category from '../../entity/Category'
 import CategoryQuerySchema from '../../schema/category/CategoryQuerySchema'
 import GetCategorySchema from '../../schema/category/GetCategorySchema'
+import ValidatorInterface from '../../service/validator/ValidatorInterface'
 
 @Service()
 @Resolver(Category)
-export default class CategoryResolver {
+export class CategoryResolver {
 
   public constructor(
     @Inject() private readonly categoryService: CategoryService,
+    @Inject('validator') private readonly validator: ValidatorInterface,
   ) {
   }
 
   @Query(_returns => Category)
-  public async category(@Args() args: GetCategorySchema): Promise<Category> {
-    return await this.categoryService.getCategory(args.categoryId)
+  public async category(
+    @Args() getCategory: GetCategorySchema,
+  ): Promise<Category> {
+    await this.validator.validate(getCategory)
+
+    return await this.categoryService.getCategory(getCategory.categoryId)
   }
 
   @Query(_returns => [ Category ])
-  public async categories(@Args() args: CategoryQuerySchema): Promise<Category[]> {
-    return await this.categoryService.queryCategories(args) as Category[]
+  public async categories(
+    @Args() categoryQuery: CategoryQuerySchema,
+  ): Promise<Category[]> {
+    return await this.categoryService.queryCategories(categoryQuery) as Category[]
   }
 }
