b987429306bef4f84ed3adc25a8982678d918e74
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-28 01:55:58 -0600

    auth resolver added

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,5 +1,7 @@
 import { CategoryResolver } from './resolvers/CategoryResolver'
+import { AuthResolver } from './resolvers/AuthResolver'
 
 export const resolvers = [
+  AuthResolver,
   CategoryResolver,
 ]
