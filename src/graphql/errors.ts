b891d0b593987186b9f520a2c13d056dbe12116d
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-04-29 00:54:02 -0600

    category name taken error added

--- src/graphql/errors.ts
+++ src/graphql/errors.ts
@@ -8,4 +8,5 @@ export const errors = {
   'UserWrongCredentialsError': 'ForbiddenError',
   'UNAUTHENTICATED': 'AuthorizationRequiredError',
   'AuthorizationFailedError': 'ForbiddenError',
+  'CategoryNameTakenError': 'ConflictError',
 }
