fc9a96d79937d6fb53704bf83d81e9d3232a4802
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-01 14:05:02 -0600

    date timestamp scalar added

--- src/graphql/scalars.ts
+++ src/graphql/scalars.ts
@@ -1,6 +1,8 @@
+import { GraphQLTimestamp } from 'graphql-scalars'
 import { ObjectIdScalar } from './scalars/ObjectIdScalar'
 import { ObjectId } from 'mongodb'
 
 export const scalars = [
   { type: ObjectId, scalar: ObjectIdScalar },
+  { type: Date, scalar: GraphQLTimestamp },
 ]
