837a92c24c8d1863679603682e072df62dfdd188
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-01 22:50:39 -0600

    update question mutation added

--- src/resolvers/QuestionResolver.ts
+++ src/resolvers/QuestionResolver.ts
@@ -7,6 +7,7 @@ import QuestionService from '../services/question/QuestionService'
 import GetQuestionSchema from '../schema/question/GetQuestionSchema'
 import QuestionQuerySchema from '../schema/question/QuestionQuerySchema'
 import QuestionSchema from '../schema/question/QuestionSchema'
+import QuestionUpdateSchema from '../schema/question/QuestionUpdateSchema'
 
 @Service()
 @Resolver(Question)
@@ -41,5 +42,18 @@ export class QuestionResolver {
     @Ctx('user') user: User,
   ): Promise<Question> {
     return await this.questionService.createQuestion(question, user)
+  }
+
+  @Authorized()
+  @Mutation(_returns => Question)
+  public async updateQuestion(
+    @Args() getQuestion: GetQuestionSchema,
+    @Arg('questionUpdate') questionUpdate: QuestionUpdateSchema,
+    @Ctx('user') user: User,
+  ): Promise<Question> {
+    await this.validator.validate(getQuestion)
+    const question = await this.questionService.getQuestion(getQuestion.questionId)
+
+    return await this.questionService.updateQuestion(question, questionUpdate, user)
   }
 }
