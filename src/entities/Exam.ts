348b6657c4a738547ba4e0f06ee1e51c77dbe1ae
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-02 00:03:29 -0600

    graphql decorators added

--- src/entities/Exam.ts
+++ src/entities/Exam.ts
@@ -2,77 +2,94 @@ import { Column, CreateDateColumn, DeleteDateColumn, Entity, ObjectIdColumn, Upd
 import { Exclude, Expose, Transform, Type } from 'class-transformer'
 import { ObjectId } from 'mongodb'
 import { IsDate, IsMongoId, IsNumber, IsOptional, IsString, Min, ValidateNested } from 'class-validator'
+import { Field, Int, ObjectType } from 'type-graphql'
+import { GraphQLTimestamp } from 'graphql-scalars'
+import { ObjectIdScalar } from '../scalars/ObjectIdScalar'
 
+@ObjectType()
 export class ExamQuestion {
 
   @IsMongoId()
   @Column()
   @Transform(({ value }: { value: ObjectId }) => value.toString())
+  @Field(_type => ObjectIdScalar)
   public question: ObjectId
 
   @IsOptional()
   @IsNumber()
-  @Column({ nullable: false })
+  @Column({ nullable: true })
+  @Field(_type => Int, { nullable: true })
   public choice?: number
 
   @IsOptional()
   @IsString()
-  @Column({ nullable: false })
+  @Column({ nullable: true })
+  @Field({ nullable: true })
   public answer?: string
 }
 
+@ObjectType()
 @Entity({ name: 'exams' })
 export default class Exam {
 
   @IsMongoId()
   @ObjectIdColumn()
   @Transform(({ value }: { value: ObjectId }) => value.toString())
+  @Field(_type => ObjectIdScalar)
   public readonly id: ObjectId
 
   @IsMongoId()
   @Column()
   @Transform(({ value }: { value: ObjectId }) => value?.toString())
+  @Field(_type => ObjectIdScalar)
   public category: ObjectId
 
   @Exclude()
   @ValidateNested({ each: true })
   @Type(() => ExamQuestion)
   @Column(() => ExamQuestion)
+  @Field(_type => [ ExamQuestion ])
   public questions: ExamQuestion[]
 
   @Min(0)
   @IsNumber({ maxDecimalPlaces: 0 })
   @Column({ default: 0 })
+  @Field(_type => Int)
   public questionNumber: number = 0
 
   @Exclude()
   @Min(0)
   @IsNumber({ maxDecimalPlaces: 0 })
   @Column({ default: 0 })
+  @Field(_type => Int)
   public correctCount: number = 0
 
   @IsOptional()
   @IsDate()
   @Column()
   @Transform(({ value }: { value: Date }) => value?.getTime())
-  public completed: Date
+  @Field(_type => GraphQLTimestamp, { nullable: true })
+  public completed?: Date
 
   @Exclude()
   @IsMongoId()
   @Column()
   @Transform(({ value }: { value: ObjectId }) => value?.toString())
+  @Field(_type => ObjectIdScalar)
   public creator: ObjectId
 
   @Exclude()
   @IsMongoId()
   @Column()
   @Transform(({ value }: { value: ObjectId }) => value?.toString())
+  @Field(_type => ObjectIdScalar)
   public owner: ObjectId
 
   @IsNumber()
   @Column()
   @CreateDateColumn()
   @Transform(({ value }: { value: Date }) => value?.getTime())
+  @Field(_type => GraphQLTimestamp)
   public created: Date
 
   @IsOptional()
@@ -80,7 +97,8 @@ export default class Exam {
   @Column()
   @UpdateDateColumn()
   @Transform(({ value }: { value: Date }) => value?.getTime())
-  public updated: Date
+  @Field(_type => GraphQLTimestamp, { nullable: true })
+  public updated?: Date
 
   @Exclude()
   @IsOptional()
@@ -88,14 +106,17 @@ export default class Exam {
   @Column()
   @DeleteDateColumn()
   @Transform(({ value }: { value: Date }) => value?.getTime())
-  public deleted: Date
+  @Field(_type => GraphQLTimestamp, { nullable: true })
+  public deleted?: Date
 
   @Expose({ name: 'questionsCount' })
+  @Field(_type => Int)
   public getQuestionsCount(): number {
     return this.questions?.length || 0
   }
 
   @Expose({ name: 'answeredCount' })
+  @Field(_type => Int)
   public getQuestionsAnsweredCount(): number {
     return (this?.questions || [])
       .filter((question: ExamQuestion): boolean => typeof question.choice === 'number' || typeof question.answer === 'string')
