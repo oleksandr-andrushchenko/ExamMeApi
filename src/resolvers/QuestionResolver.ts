3a7e113281bd16e7c47b783563d8bc69bdb4b3d2
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-01 21:46:37 -0600

    question query added

--- src/resolvers/QuestionResolver.ts
+++ src/resolvers/QuestionResolver.ts
@@ -1,8 +1,10 @@
 import { Inject, Service } from 'typedi'
-import { Arg, Authorized, Ctx, Mutation, Resolver } from 'type-graphql'
+import { Arg, Args, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql'
 import User from '../entities/User'
+import ValidatorInterface from '../services/validator/ValidatorInterface'
 import Question from '../entities/Question'
 import QuestionService from '../services/question/QuestionService'
+import GetQuestionSchema from '../schema/question/GetQuestionSchema'
 import QuestionSchema from '../schema/question/QuestionSchema'
 
 @Service()
@@ -11,7 +13,17 @@ export class QuestionResolver {
 
   public constructor(
     @Inject() private readonly questionService: QuestionService,
+    @Inject('validator') private readonly validator: ValidatorInterface,
   ) {
+  }
+
+  @Query(_returns => Question)
+  public async question(
+    @Args() getQuestion: GetQuestionSchema,
+  ): Promise<Question> {
+    await this.validator.validate(getQuestion)
+
+    return await this.questionService.getQuestion(getQuestion.questionId)
   }
 
   @Authorized()
