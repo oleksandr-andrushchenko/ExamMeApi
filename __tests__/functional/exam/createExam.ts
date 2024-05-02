92b7445b5bf067bc2e4698353a5035a18f6f68b1
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-02 01:04:28 -0600

    graphql cases added

--- __tests__/functional/exam/createExam.ts
+++ __tests__/functional/exam/createExam.ts
@@ -1,11 +1,13 @@
 import { describe, expect, test } from '@jest/globals'
 import request from 'supertest'
-import { auth, error, fixture, load, server as app } from '../../index'
+import { auth, fixture, load, server as app } from '../../index'
 import User from '../../../src/entities/User'
 import Exam from '../../../src/entities/Exam'
 import Category from '../../../src/entities/Category'
 import { ObjectId } from 'mongodb'
 import ExamPermission from '../../../src/enums/exam/ExamPermission'
+// @ts-ignore
+import { addExamMutation } from '../../graphql/exam/addExamMutation'
 
 describe('Create exam', () => {
   test('Unauthorized', async () => {
@@ -50,6 +52,60 @@ describe('Create exam', () => {
     expect(res.status).toEqual(201)
     expect(res.body).toHaveProperty('id')
     const id = new ObjectId(res.body.id)
+    expect(await load<Exam>(Exam, id)).toMatchObject({ id })
+  })
+  test('Unauthorized (GraphQL)', async () => {
+    const category = await fixture<Category>(Category)
+    const res = await request(app).post('/graphql')
+      .send(addExamMutation({ exam: { category: category.id.toString() } }))
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject(graphqlError('AuthorizationRequiredError'))
+  })
+  test('Bad request (empty body) (GraphQL)', async () => {
+    const user = await fixture<User>(User)
+    const token = (await auth(user)).token
+    const res = await request(app).post('/graphql')
+      .send(addExamMutation({ exam: {} as CreateExamSchema }))
+      .auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject(graphqlError('BadRequestError'))
+  })
+  test('Forbidden (GraphQL)', async () => {
+    const user = await fixture<User>(User)
+    const token = (await auth(user)).token
+    const category = await fixture<Category>(Category)
+    const res = await request(app).post('/graphql')
+      .send(addExamMutation({ exam: { category: category.id.toString() } }))
+      .auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject(graphqlError('ForbiddenError'))
+  })
+  test('Conflict (exam taken) (GraphQL)', async () => {
+    const user = await fixture<User>(User, { permissions: [ ExamPermission.CREATE ] })
+    const token = (await auth(user)).token
+    const exam = await fixture<Exam>(Exam, { creator: user.id })
+    const res = await request(app).post('/graphql')
+      .send(addExamMutation({ exam: { category: exam.category.toString() } }))
+      .auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject(graphqlError('ConflictError'))
+  })
+  test('Created (GraphQL)', async () => {
+    const user = await fixture<User>(User, { permissions: [ ExamPermission.CREATE ] })
+    const token = (await auth(user)).token
+    const category = await fixture<Category>(Category)
+    const res = await request(app).post('/graphql')
+      .send(addExamMutation({ exam: { category: category.id.toString() } }))
+      .auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toHaveProperty('data')
+    expect(res.body.data).toHaveProperty('addExam')
+    const id = new ObjectId(res.body.data.addExam.id)
     expect(await load<Exam>(Exam, id)).toMatchObject({ id })
   })
 })
