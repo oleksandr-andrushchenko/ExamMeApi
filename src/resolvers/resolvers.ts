1ccd4b2c9c9e0a7a4fbe17f72afe621eaf46a0df
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-01 21:13:12 -0600

    question resolver added

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -1,7 +1,9 @@
 import { CategoryResolver } from './CategoryResolver'
 import { AuthResolver } from './AuthResolver'
+import { QuestionResolver } from './QuestionResolver'
 
 export const resolvers = [
   AuthResolver,
   CategoryResolver,
+  QuestionResolver,
 ]
