ceedbbb3d6b212074cedd1cb4f2953cff9c2ebcf
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-03 00:45:07 -0600

    exam id type fixed

---
+++ __tests__/graphql/exam/removeExamMutation.ts
@@ -0,0 +1,12 @@
+import GetExamSchema from '../../../src/schema/exam/GetExamSchema'
+
+export const removeExamMutation = (variables: GetExamSchema) => {
+  return {
+    query: `
+      mutation RemoveExam($examId: ID!) {
+        removeExam(examId: $examId)
+      }
+  `,
+    variables,
+  }
+}
