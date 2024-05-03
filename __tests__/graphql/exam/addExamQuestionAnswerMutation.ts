34aae923c4da697c31b3ec4078e1036c68fd88e3
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-03 00:37:12 -0600

    add exam question answer graphql mutation added

---
+++ __tests__/graphql/exam/addExamQuestionAnswerMutation.ts
@@ -0,0 +1,17 @@
+import CreateExamQuestionAnswerSchema from '../../../src/schema/exam/CreateExamQuestionAnswerSchema'
+import GetExamQuestionSchema from '../../../src/schema/exam/GetExamQuestionSchema'
+
+export const addExamQuestionAnswerMutation = (variables: GetExamQuestionSchema & {
+  examQuestionAnswer: CreateExamQuestionAnswerSchema
+}, fields: string[] = [ 'difficulty', 'question', 'difficulty' ]) => {
+  return {
+    query: `
+      mutation AddExamQuestionAnswer($examId: ID!, $question: Int!, $examQuestionAnswer: CreateExamQuestionAnswerSchema!) {
+        addExamQuestionAnswer(examId: $examId, question: $question, examQuestionAnswer: $examQuestionAnswer) {
+          ${ fields.join('\r') }
+        }
+      }
+  `,
+    variables,
+  }
+}
