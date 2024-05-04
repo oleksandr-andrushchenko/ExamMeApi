059c450b0dd1ffb726e58954aef57f5135d20bcc
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-04 16:21:03 -0600

    me resovler added

---
+++ src/resolvers/MeResolver.ts
@@ -0,0 +1,48 @@
+import { Inject, Service } from 'typedi'
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql'
+import User from '../entities/User'
+import MeService from '../services/user/MeService'
+import MeSchema from '../schema/user/MeSchema'
+import MeUpdateSchema from '../schema/user/MeUpdateSchema'
+
+@Service()
+@Resolver(User)
+export class MeResolver {
+
+  public constructor(
+    @Inject() private readonly meService: MeService,
+  ) {
+  }
+
+  @Mutation(_returns => User)
+  public async addMe(
+    @Arg('me') me: MeSchema,
+  ): Promise<User> {
+    return await this.meService.createMe(me)
+  }
+
+  @Authorized()
+  @Query(_returns => User)
+  public async me(
+    @Ctx('user') user: User,
+  ): Promise<User> {
+    return user
+  }
+
+  @Authorized()
+  @Mutation(_returns => User)
+  public async updateMe(
+    @Arg('meUpdate') meUpdate: MeUpdateSchema,
+    @Ctx('user') user: User,
+  ): Promise<User> {
+    return await this.meService.updateMe(meUpdate, user)
+  }
+
+  @Authorized()
+  @Mutation(_returns => Boolean)
+  public async removeMe(
+    @Ctx('user') user: User,
+  ): Promise<void> {
+    await this.meService.deleteMe(user)
+  }
+}
--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -2,10 +2,12 @@ import { CategoryResolver } from './CategoryResolver'
 import { AuthResolver } from './AuthResolver'
 import { QuestionResolver } from './QuestionResolver'
 import { ExamResolver } from './ExamResolver'
+import { MeResolver } from './MeResolver'
 
 export const resolvers = [
   AuthResolver,
   CategoryResolver,
   QuestionResolver,
   ExamResolver,
+  MeResolver,
 ]
