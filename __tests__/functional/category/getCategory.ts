f8303640e1deb015cf8cd971646dd579f2dd7a44
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-04 22:38:14 -0600

    more assertions added; soft refactoring

--- __tests__/functional/category/getCategory.ts
+++ __tests__/functional/category/getCategory.ts
@@ -14,8 +14,7 @@ describe('Get category', () => {
     expect(res.body).toMatchObject(error('NotFoundError'))
   })
   test('Bad request (invalid id)', async () => {
-    const id = 'invalid'
-    const res = await request(app).get(`/categories/${ id.toString() }`)
+    const res = await request(app).get('/categories/invalid')
 
     expect(res.status).toEqual(400)
     expect(res.body).toMatchObject(error('BadRequestError'))
@@ -34,8 +33,14 @@ describe('Get category', () => {
       rating: category.rating,
       owner: category.owner.toString(),
       created: category.created.getTime(),
-      updated: category.updated?.getTime(),
     })
+
+    if (category.updated) {
+      expect(res.body).toMatchObject({
+        updated: category.updated.getTime(),
+      })
+    }
+
     expect(res.body).not.toHaveProperty([ 'creator', 'deleted' ])
   })
   test('Not found (GraphQL)', async () => {
@@ -47,8 +52,7 @@ describe('Get category', () => {
     expect(res.body).toMatchObject(graphqlError('NotFoundError'))
   })
   test('Bad request (invalid id) (GraphQL)', async () => {
-    const id = 'invalid'
-    const variables = { categoryId: id.toString() }
+    const variables = { categoryId: 'invalid' }
     const res = await request(app).post('/graphql').send(categoryQuery(variables))
 
     expect(res.status).toEqual(200)
@@ -56,7 +60,7 @@ describe('Get category', () => {
   })
   test('Found (GraphQL)', async () => {
     const category = await fixture<Category>(Category)
-    const fields = [ 'id', 'name', 'questionCount', 'requiredScore', 'voters', 'rating' ]
+    const fields = [ 'id', 'name', 'questionCount', 'requiredScore', 'voters', 'rating', 'created', 'updated' ]
     const res = await request(app).post('/graphql').send(categoryQuery({ categoryId: category.id.toString() }, fields))
 
     expect(res.status).toEqual(200)
@@ -69,8 +73,12 @@ describe('Get category', () => {
           requiredScore: category.requiredScore,
           voters: category.voters,
           rating: category.rating,
+          created: category.created.getTime(),
+          updated: category.updated?.getTime() ?? null,
         },
       },
     })
+
+    expect(res.body.data.category).not.toHaveProperty([ 'creator', 'deleted' ])
   })
 })
