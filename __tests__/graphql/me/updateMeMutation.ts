5a3495de108663fd1346b43950300614e01522f9
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-04 19:22:18 -0600

    update me graphql mutation added

---
+++ __tests__/graphql/me/updateMeMutation.ts
@@ -0,0 +1,14 @@
+import MeUpdateSchema from '../../../src/schema/user/MeUpdateSchema'
+
+export const updateMeMutation = (variables: { meUpdate: MeUpdateSchema }, fields: string[] = [ 'id' ]) => {
+  return {
+    query: `
+      mutation UpdateMe($meUpdate: MeUpdateSchema!) {
+        updateMe(meUpdate: $meUpdate) {
+          ${ fields.join('\r') }
+        }
+      }
+  `,
+    variables,
+  }
+}
