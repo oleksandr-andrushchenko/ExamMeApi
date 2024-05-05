9371186856b0d306333f29e78cdc4fcabc0a76a3
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-05 00:18:26 -0600

    grapql decorators added

--- src/schema/user/UserSchema.ts
+++ src/schema/user/UserSchema.ts
@@ -1,11 +1,14 @@
 import { ArrayUnique, IsEnum, IsOptional } from 'class-validator'
 import Permission from '../../enums/Permission'
 import MeSchema from './MeSchema'
+import { Field, InputType } from 'type-graphql'
 
+@InputType()
 export default class UserSchema extends MeSchema {
 
   @IsOptional()
   @IsEnum(Permission, { each: true })
   @ArrayUnique()
-  public readonly permissions: Permission[]
+  @Field(_type => [ String ])
+  public readonly permissions?: Permission[] = [ Permission.REGULAR ]
 }
