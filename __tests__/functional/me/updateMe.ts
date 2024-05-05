31e8fdd1af3aa1f7faaf7c4f748cfb178beecca5
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-04 19:22:38 -0600

    update me graphql cases added

--- __tests__/functional/me/updateMe.ts
+++ __tests__/functional/me/updateMe.ts
@@ -1,7 +1,10 @@
 import { describe, expect, test } from '@jest/globals'
 import request from 'supertest'
-import { auth, error, fixture, load, server as app } from '../../index'
+import { auth, error, fixture, graphqlError, load, server as app } from '../../index'
 import User from '../../../src/entities/User'
+// @ts-ignore
+import { updateMeMutation } from '../../graphql/me/updateMeMutation'
+import MeUpdateSchema from '../../../src/schema/user/MeUpdateSchema'
 
 describe('Update me', () => {
   test('Unauthorized', async () => {
@@ -36,5 +39,61 @@ describe('Update me', () => {
     expect(res.status).toEqual(205)
     expect(res.body).toEqual('')
     expect(await load<User>(User, user.id)).toMatchObject(schema)
+  })
+  test('Unauthorized (GraphQL)', async () => {
+    const res = await request(app).post('/graphql')
+      .send(updateMeMutation({ meUpdate: { name: 'any' } }))
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject(graphqlError('AuthorizationRequiredError'))
+  })
+  test('Bad request (empty body) (GraphQL)', async () => {
+    const user = await fixture<User>(User)
+    const token = (await auth(user)).token
+    const res = await request(app).post('/graphql')
+      .send(updateMeMutation({} as { meUpdate: MeUpdateSchema }))
+      .auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject(graphqlError('BadRequestError'))
+  })
+  test('Conflict (GraphQL)', async () => {
+    const user1 = await fixture<User>(User)
+    const user = await fixture<User>(User)
+    const token = (await auth(user)).token
+    const res = await request(app).post('/graphql')
+      .send(updateMeMutation({ meUpdate: { email: user1.email } }))
+      .auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject(graphqlError('ConflictError'))
+  })
+  test('Updated (GraphQL)', async () => {
+    const user = await fixture<User>(User)
+    const token = (await auth(user)).token
+    const meUpdate = { name: 'any' }
+    const now = new Date().getTime()
+    const res = await request(app).post('/graphql')
+      .send(updateMeMutation({ meUpdate }, [ 'id', 'name', 'email', 'permissions', 'created', 'updated' ]))
+      .auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject({
+      data: {
+        updateMe: {
+          ...{
+            id: user.id.toString(),
+            name: user.name,
+            email: user.email,
+            permissions: user.permissions,
+            created: user.created.getTime(),
+          },
+          ...meUpdate,
+        },
+      },
+    })
+    expect(res.body.data.updateMe).toHaveProperty([ 'updated' ])
+    expect(res.body.data.updateMe.updated).toBeGreaterThanOrEqual(now)
+    expect(res.body.data.updateMe).not.toHaveProperty([ 'password', 'creator', 'deleted' ])
   })
 })
