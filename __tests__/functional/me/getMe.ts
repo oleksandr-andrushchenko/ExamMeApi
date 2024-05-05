b8ff5e90573a1590fdb1873fb840037ebb71e237
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-04 19:05:46 -0600

    graphql cases added

--- __tests__/functional/me/getMe.ts
+++ __tests__/functional/me/getMe.ts
@@ -1,7 +1,9 @@
 import { describe, expect, test } from '@jest/globals'
 import request from 'supertest'
-import { auth, error, fixture, server as app } from '../../index'
+import { auth, error, fixture, graphqlError, server as app } from '../../index'
 import User from '../../../src/entities/User'
+// @ts-ignore
+import { meQuery } from '../../graphql/me/meQuery'
 
 describe('Get me', () => {
   test('Unauthorized', async () => {
@@ -17,5 +19,34 @@ describe('Get me', () => {
 
     expect(res.status).toEqual(200)
     expect(res.body).toMatchObject({ id: user.id.toString(), name: user.name })
+  })
+  test('Unauthorized (GraphQL)', async () => {
+    const res = await request(app).post('/graphql')
+      .send(meQuery())
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject(graphqlError('AuthorizationRequiredError'))
+  })
+  test('Found (GraphQL)', async () => {
+    const user = await fixture<User>(User)
+    const token = (await auth(user)).token
+    const res = await request(app).post('/graphql')
+      .send(meQuery([ 'id', 'name', 'email', 'permissions', 'created', 'updated' ]))
+      .auth(token, { type: 'bearer' })
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toMatchObject({
+      data: {
+        me: {
+          id: user.id.toString(),
+          name: user.name,
+          email: user.email,
+          permissions: user.permissions,
+          created: user.created.getTime(),
+          updated: user.updated?.getTime(),
+        },
+      },
+    })
+    expect(res.body.data.me).not.toHaveProperty([ 'password', 'creator', 'deleted' ])
   })
 })
