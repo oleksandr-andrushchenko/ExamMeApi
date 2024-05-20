339ca52b696cac2722a3c774144569de0983e4e8
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-05-19 20:50:07 -0600

    remove duplicated filter

--- src/models/Cursor.ts
+++ src/models/Cursor.ts
@@ -79,8 +79,6 @@ export default class Cursor<Entity> {
 
     sort['_id'] = order.direction
 
-    where['deletedAt'] = { $exists: false }
-
     const data = await this.repository.find({ where, take: this.pagination.size, order: sort })
 
     if (this.pagination.prevCursor) {
