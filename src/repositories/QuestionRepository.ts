39279fdd24c8584f95125b2db3abad766f785ed6
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-07-24 17:26:00 -0600

    count by category method added; count by category and no owner method added

--- src/repositories/QuestionRepository.ts
+++ src/repositories/QuestionRepository.ts
@@ -1,11 +1,20 @@
 import Repository from '../decorators/Repository'
 import Question from '../entities/Question'
 import EntityRepository from './EntityRepository'
+import Category from '../entities/Category'
 
 @Repository(Question)
 export default class QuestionRepository extends EntityRepository<Question> {
 
   public async findOneByTitle(title: string): Promise<Question | null> {
     return await this.findOneBy({ title })
+  }
+
+  public async countByCategory(category: Category): Promise<number> {
+    return await this.count({ categoryId: category.id })
+  }
+
+  public async countByCategoryAndNoOwner(category: Category): Promise<number> {
+    return await this.count({ categoryId: category.id, ownerId: { $exists: false } })
   }
 }
