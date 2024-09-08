609c4cf694e53d33dfda5779436f60eab73068c2
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2024-09-08 14:40:16 -0600

    soft non-func refactoring

--- src/decorators/EventSubscriber.ts
+++ src/decorators/EventSubscriber.ts
@@ -5,9 +5,9 @@ import { Event } from '../enums/Event'
 export default function EventSubscriber(event: Event): ClassDecorator {
   return <T = Constructable<unknown>>(subscriber: T): void => {
     const eventDispatcher = Container.get(EventDispatcher)
-    eventDispatcher.on(event, (data: any) => {
+    eventDispatcher.on(event, async (data: any) => {
       const eventSubscriber: { handle: (data: any) => void } = Container.get(subscriber)
-      eventSubscriber.handle(data)
+      await eventSubscriber.handle(data)
     })
   }
 }
